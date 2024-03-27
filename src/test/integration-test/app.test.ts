634461bf1e4a237a90b9d4be395d35b4e0265e31
Jaroon Umpornpoolsuk <jaroon_ump_fth@tdem.toyota-asia.com>
2024-03-27 16:11:26 +0700

    lab03-question-01-and-02-fix

--- src/test/integration-test/app.test.ts
+++ src/test/integration-test/app.test.ts
@@ -10,90 +10,97 @@ describe('Product Management', () => {
     // const newProduct = { name: 'Product 1', description: 'Product 1 description', price: 100 };
 
     // // เพิ่มข้อมูลผลิตภัณฑ์ใหม่
-    // const xxxxx = await xxxxx(xxxxx)
-    //   .xxxxxx('xxxxx')
-    //   .xxxxx(xxxxx);
+    // const response = await xxxxx(xxxxx)
+    //   .post('/products')
+    //   .send(newProduct);
 
     // // ตรวจสอบผลลัพธ์การเพิ่มข้อมูลผลิตภัณฑ์ใหม่ : โดยตรวจสอบจาก response.status เป็น 201 หรือไม่
-    // xxxxx(xxxxx).xxxxx(xxxxx);
+    // expect(xxxxx).xxxxx(201);
 
     // // ตรวจสอบผลลัพธ์ : ใน response.body ที่ได้มานั้น มี property 'id' หรือไม่
-    // xxxxx(xxxxx).xxxxx('xxxxx');
+    // expect(xxxxx).toHaveProperty('xxxxx');
 
     // // บันทึก ID ผลิตภัณฑ์เพื่อใช้ในการทดสอบต่อไป
-    // xxxxx = xxxxx.xxxxx.xxxxx;
+    // productId = response.body.id;
   });
 
-  it('should return a product', async () => {
+  it('should get a product', async () => {
     // // นำ ID ที่ได้บันทึกไว้ (จากการทดสอบ 'should add a new product') มาใช้ในการค้นหาผลิตภัณฑ์
-    // const xxxxxx = await xxxxx(xxxxx)
-    //   .xxxxx(`xxxxx`);
+    // const response = await xxxxx(xxxxx)
+    //   .get(`/products/${productId}`);
 
     // // ตรวจสอบผลลัพธ์การค้นหาข้อมูลผลิตภัณฑ์ : โดยตรวจสอบจาก response.status เป็น 200 หรือไม่
-    // xxxxx(xxxxx).xxxxx(xxxxx);
+    // expect(xxxxx).xxxxx(200);
 
     // // ตรวจสอบผลลัพธ์ : ใน response.body ที่ได้มานั้น มี property 'id' ตรงกับ productId ที่ใช้ในการค้นหาผลิตภัณฑ์ หรือไม่
-    // xxxxx(xxxxx).xxxxx('xxxxx', xxxxx);
+    // expect(xxxxx).toHaveProperty('id', productId);
   });
 
   it('should update the product', async () => {
     // // กำหนดข้อมูลผลิตภัณฑ์ที่จะอัปเดต
-    // xxxxx
+    // const updatedProduct = { name: 'Updated Product', description: 'Updated description', price: 150 }
 
     // // อัปเดตข้อมูลผลิตภัณฑ์
-    // xxxxx
+    // const response = await xxxxx(xxxxx)
+      // .put(`/products/${productId}`)
+      // .send(updatedProduct);
 
     // // ตรวจสอบผลลัพธ์การอัปเดตข้อมูลผลิตภัณฑ์ : โดยตรวจสอบจาก response.status เป็น 200 หรือไม่
-    // xxxxx
+    // expect(xxxxx).xxxxx(xxxxx);
   });
 
   it('should delete the product', async () => {
     // // ลบข้อมูลผลิตภัณฑ์
-    // xxxxx
+    // const response = await xxxxx(xxxxx)
+      // .delete(`/products/${productId}`);
 
     // // ตรวจสอบผลลัพธ์การลบข้อมูลผลิตภัณฑ์ : โดยตรวจสอบจาก response.status เป็น 204 หรือไม่
-    // xxxxx
+    // expect(xxxxx).xxxxx(xxxxx);
   });
 });
 
 describe('GET /products/:id with non-existing id', () => {
-    it('should return 404 for a non-existing product', async () => {
-        // // ค้นหาผลิตภัณฑ์ที่ไม่มีอยู่ในระบบ เช่น 9999
-        // const xxxxx = await xxxxx(xxxxxx).xxxxxx('xxxxx');
+  it('should return 404 for a non-existing product', async () => {
+    // // ค้นหาผลิตภัณฑ์ที่ไม่มีอยู่ในระบบ เช่น 9999
+    // const response = await xxxxx(xxxxx)
+    //   .get('/products/9999');
 
-        // // ตรวจสอบผลลัพธ์การค้นหาผลิตภัณฑ์ : โดยตรวจสอบจาก response.status เป็น 404 หรือไม่
-        // xxxxxx(xxxxx).xxxxx(xxxxx);
+    // // ตรวจสอบผลลัพธ์การค้นหาผลิตภัณฑ์ : โดยตรวจสอบจาก response.status เป็น 404 หรือไม่
+    // expect(xxxxx).xxxxx(xxxxx);
 
-        // // ตรวจสอบผลลัพธ์ : ใน response.text ที่ได้มานั้น ต้องเป็น 'Product not found'
-        // xxxxx(xxxxxx).xxxxx('xxxxx');
-    });
+    // // ตรวจสอบผลลัพธ์ : ใน response.text ที่ได้มานั้น ต้องเป็น 'Product not found'
+    // expect(xxxxx).xxxxx(xxxxxx);
+  });
 });
 
 describe('PUT /products/:id with non-existing id', () => {
-    it('should return 404 when trying to update a non-existing product', async () => {
-        // // กำหนดข้อมูลผลิตภัณฑ์ที่จะอัปเดต
-        // xxxxx
+  it('should return 404 when trying to update a non-existing product', async () => {
+    // // กำหนดข้อมูลผลิตภัณฑ์ที่จะอัปเดต
+    // const productUpdate = { name: 'New Name', description: 'New Description', price: 100 };
 
-        // // อัปเดตผลิตภัณฑ์ที่ไม่มีอยู่ในระบบ
-        // xxxxx
+    // // อัปเดตผลิตภัณฑ์ที่ไม่มีอยู่ในระบบ
+    // const response = await xxxxx(xxxxx)
+      // .put('/products/9999')
+      // .send(productUpdate);
 
-        // // ตรวจสอบผลลัพธ์การอัปเดตผลิตภัณฑ์ : โดยตรวจสอบจาก response.status เป็น 404 หรือไม่
-        // xxxxx
+    // // ตรวจสอบผลลัพธ์การอัปเดตผลิตภัณฑ์ : โดยตรวจสอบจาก response.status เป็น 404 หรือไม่
+    // expect(xxxxx).xxxxx(xxxxx);
 
-        // // ตรวจสอบผลลัพธ์ : ใน response.text ที่ได้มานั้น ต้องเป็น 'Product not found'
-        // xxxxx
-    });
+    // // ตรวจสอบผลลัพธ์ : ใน response.text ที่ได้มานั้น ต้องเป็น 'Product not found'
+    // expect(xxxxx).xxxxxx(xxxxx);
+  });
 });
 
 describe('DELETE /products/:id with non-existing id', () => {
-    it('should return 404 when trying to delete a non-existing product', async () => {
-        // // ลบผลิตภัณฑ์ที่ไม่มีอยู่ในระบบ เช่น 9999
-        // xxxxx
+  it('should return 404 when trying to delete a non-existing product', async () => {
+    // // ลบผลิตภัณฑ์ที่ไม่มีอยู่ในระบบ เช่น 9999
+    // const response = await xxxxx(xxxxx)
+    //   .delete('/products/9999');
 
-        // // ตรวจสอบผลลัพธ์การลบผลิตภัณฑ์ : โดยตรวจสอบจาก response.status เป็น 404 หรือไม่
-        // xxxxx
+    // // ตรวจสอบผลลัพธ์การลบผลิตภัณฑ์ : โดยตรวจสอบจาก response.status เป็น 404 หรือไม่
+    // expect(xxxxx).xxxxx(xxxxx);
 
-        // // ตรวจสอบผลลัพธ์ : ใน response.text ที่ได้มานั้น ต้องเป็น 'Product not found'
-        // xxxxx
-    });
+    // // ตรวจสอบผลลัพธ์ : ใน response.text ที่ได้มานั้น ต้องเป็น 'Product not found'
+    // expect(xxxxx).xxxxx(xxxxx);
+  });
 });
--- src/test/integration-test/app2.test.ts
+++ src/test/integration-test/app2.test.ts
@@ -9,17 +9,17 @@ describe('User Management API', () => {
         // await request(app).post('/reset');
 
         // // ลงทะเบียนผู้ใช้สำหรับการทดสอบ
-        // const registerRes = await request(app)
-        //     .xxxxx('xxxxx')
-        //     .xxxxx({ username: 'testuser', password: 'testpass' });
+        // const registerRes = await xxxxx(xxxxx)
+        //     .post('/register')
+        //     .send({ username: 'testuser', password: 'testpass' });
 
         // // ตรวจสอบการลงทะเบียนสำเร็จ
-        // expect(xxxxx).toEqual(xxxxx);
+        // expect(registerRes.statusCode).xxxxx(201);
 
         // // เข้าสู่ระบบเพื่อรับ accessToken
-        // const loginRes = await request(app)
-        //     .xxxxx('xxxxx')
-        //     .xxxxx({ username: 'testuser', password: 'testpass' });
+        // const loginRes = await xxxxx(xxxxx)
+        //     .post('/login')
+        //     .send({ username: 'testuser', password: 'testpass' });
 
         // // บันทึก accessToken สำหรับใช้ในการทดสอบ
         // accessToken = loginRes.body.accessToken;
@@ -28,23 +28,23 @@ describe('User Management API', () => {
     // ทดสอบการลงทะเบียนผู้ใช้ใหม่
     test('should register a new user', async () => {
         // // Call API เพื่อลงทะเบียนผู้ใช้ใหม่
-        // const res = await request(app)
-        //     .xxxxx('xxxxx')
-        //     .xxxxx({ username: 'newuser', password: 'newpass' });
+        // const res = await xxxxx(xxxxx)
+        //     .post('/register')
+        //     .send({ username: 'newuser', password: 'newpass' });
 
-        // // ตรวจสอบการลงทะเบียนผู้ใช้ใหม่ : ว่าขึ้น statusCode 200 หรือไม่
-        // xxxxx(xxxxx).xxxxx(xxxxx);
+        // // ตรวจสอบการลงทะเบียนผู้ใช้ใหม่ : ว่าขึ้น statusCode 201 หรือไม่
+        // expect(res.xxxxx).xxxxx(xxxxx);
 
         // // ตรวจสอบการลงทะเบียนผู้ใช้ใหม่ : ว่าขึ้น text 'User registered successfully' หรือไม่
-        // xxxxx(xxxxx).xxxxx('xxxxx');
+        // expect(res.text).xxxxx('User registered successfully');
     });
 
     // ทดสอบการเข้าสู่ระบบและรับ token
     test('should login the user and return a token', async () => {
         // // Call API เพื่อเข้าสู่ระบบ
-        // const res = await request(app)
-        //     .xxxxx('xxxxx')
-        //     .xxxxx({ xxxxx: 'xxxxx', xxxxx: 'xxxxx' });
+        // const res = await xxxxx(xxxxx)
+        //     .post('/login')
+        //     .send({ username: 'testuser', password: 'testpass' });
 
         // // ตรวจสอบการเข้าสู่ระบบ : ว่าขึ้น statusCode 200 หรือไม่
         // xxxxx(res.xxxxx).xxxxx(xxxxx);
@@ -56,110 +56,113 @@ describe('User Management API', () => {
     // ทดสอบการเข้าสู่ระบบด้วยชื่อผู้ใช้ที่ไม่มีอยู่จริง
     test('should return 401 for a non-existing user login attempt', async () => {
         // // Call API เพื่อเข้าสู่ระบบ
-        // const xxxxx = await xxxxx(xxxxx)
-        //     .xxxxxx('xxxxx')
-        //     .xxxxx({
+        // const res = await xxxxx(xxxxx)
+        //     .post('/login')
+        //     .send({
         //         username: 'nonExistingUser', // ใช้ชื่อผู้ใช้ที่ไม่มีอยู่จริง
         //         password: 'anyPassword',
         //     });
 
         // // ตรวจสอบการเข้าสู่ระบบ : ว่าขึ้น statusCode 401 หรือไม่
-        // xxxxxx(xxxxx.xxxxx).xxxxx(xxxxxx);
+        // expect(res.xxxxx).xxxxx(xxxxx);
 
         // // ตรวจสอบการเข้าสู่ระบบ : ใน res.text ที่ได้มานั้น ต้องเป็น 'Cannot find user or password is incorrect'
-        // xxxxxx(xxxxx.xxxxxx).xxxxxx('xxxxxx');
+        // expect(res.text).xxxxx('Cannot find user or password is incorrect');
     });
 
     // ทดสอบการเข้าสู่ระบบด้วยรหัสผ่านที่ไม่ถูกต้อง
     test('should return 401 for an incorrect password login attempt', async () => {
         // // Call API เพื่อเข้าสู่ระบบ
-        // const xxxxxx = await xxxxxx(xxxxxx)
-        //     .xxxxx('xxxxx')
-        //     .xxxxx({
-        //         xxxxx: 'testuser', // ใช้ชื่อผู้ใช้ที่มีอยู่จริง
-        //         xxxxx: 'incorrectPassword', // ใช้รหัสผ่านที่ไม่ถูกต้อง
+        // const res = await xxxxxx(xxxxxx)
+        //     .post('/login')
+        //     .send({
+        //         username: 'testuser', // ใช้ชื่อผู้ใช้ที่มีอยู่จริง
+        //         password: 'incorrectPassword', // ใช้รหัสผ่านที่ไม่ถูกต้อง
         //     });
 
         // // ตรวจสอบการเข้าสู่ระบบ : ว่าขึ้น statusCode 401 หรือไม่
-        // xxxxx(xxxxx).xxxxx(xxxxx);
+        // expect(res.xxxxx).xxxxx(xxxxx);
 
         // // ตรวจสอบการเข้าสู่ระบบ : ใน res.text ที่ได้มานั้น ต้องเป็น 'Cannot find user or password is incorrect'
-        // xxxxx(xxxxx).xxxxx('xxxxx');
+        // expect(res.text).xxxxx('Cannot find user or password is incorrect');
     });
 
     // ทดสอบการดูข้อมูลโปรไฟล์
     test('should return the user profile', async () => {
         // // Call API เพื่อดูข้อมูลโปรไฟล์
-        // const xxxxxx = await xxxxx(xxxxx)
-        //     .xxxxxx('xxxxxx')
+        // const res = await xxxxx(xxxxx)
+        //     .get('/myProfile')
         //     .set('Authorization', `Bearer ${accessToken}`);
 
         // // ตรวจสอบการดูข้อมูลโปรไฟล์ : ว่าขึ้น statusCode 200 หรือไม่
-        // xxxxx(xxxxx).xxxxx(xxxxx);
+        // expect(xxxxx).xxxxx(xxxxx);
 
         // // ตรวจสอบการดูข้อมูลโปรไฟล์ : ใน res.body ที่ได้มานั้น ต้องเป็น property 'username'
-        // xxxxx(xxxxx).xxxxx('username', 'testuser');
+        // expect(xxxxx).xxxxx('username', 'testuser');
     });
 
     // ทดสอบการดูข้อมูลโปรไฟล์ ที่ไม่มีอยู่จริง
     test('should return the user profile (Not Found)', async () => {
         // // ล้างข้อมูลผู้ใช้ทั้งหมด
-        // await request(app).xxxxx('xxxxx');
+        // await request(app).post('/reset');
 
         // // Call API เพื่อดูข้อมูลโปรไฟล์
-        // const xxxxxx = await xxxxx(xxxxx)
-        //     .xxxxxx('xxxxxx')
-        //     .xxxxxx('xxxxx', `xxxxxx`);
+        // const res = await xxxxx(xxxxx)
+        //     .get('/myProfile')
+        //     .set('Authorization', `Bearer ${accessToken}`);
 
         // // ตรวจสอบการดูข้อมูลโปรไฟล์ : ว่าขึ้น statusCode 404 หรือไม่
-        // xxxxx(xxxxx).xxxxx(xxxxx);
+        // expect(res.xxxxx).xxxxx(xxxxx);
 
         // // ตรวจสอบการดูข้อมูลโปรไฟล์ : ใน res.text ที่ได้มานั้น ต้องเป็น 'User not found'
-        // xxxxx(xxxxx).xxxxx('xxxxx');
+        // expect(res.text).xxxxx('User not found');
     });
 
     // ทดสอบการแก้ไขข้อมูลโปรไฟล์
     test('should update the user profile', async () => {
         // // Call API เพื่อแก้ไขข้อมูลโปรไฟล์
-        // const xxxxx = await xxxxx(xxxxx)
-        //     .xxxxx('xxxxx')
+        // const res = await xxxxx(xxxxx)
+        //     .put('/myProfile')
         //     .send({ username: 'updatedTestuser' })
         //     .set('Authorization', `Bearer ${accessToken}`);
 
         // // ตรวจสอบการแก้ไขข้อมูลโปรไฟล์ : ว่าขึ้น statusCode 200 หรือไม่
-        // xxxxx(xxxxx).xxxxx(xxxxx);
+        // expect(xxxxx).xxxxx(xxxxx);
 
         // // ตรวจสอบการแก้ไขข้อมูลโปรไฟล์ : ใน res.body ที่ได้มานั้น ต้องเป็น property 'username'
-        // xxxxx(xxxxx).xxxxx('username', 'updatedTestuser');
+        // expect(xxxxx).xxxxx('username', 'updatedTestuser');
     });
 
     // ทดสอบการแก้ไขข้อมูลโปรไฟล์ ที่ไม่มีอยู่จริง
     test('should update the user profile (Not Found)', async () => {
         // // ล้างข้อมูลผู้ใช้ทั้งหมด
-        // xxxxxx
+        // await xxxxx(xxxxx).xxxxx('xxxxx');
 
         // // Call API เพื่อแก้ไขข้อมูลโปรไฟล์
-        // xxxxx
+        // const res = await xxxxx(xxxxx)
+        //     .put('/myProfile')
+        //     .send({ username: 'updatedTestuser' })
+        //     .set('Authorization', `Bearer ${accessToken}`);
 
         // // ตรวจสอบการแก้ไขข้อมูลโปรไฟล์ : ว่าขึ้น statusCode 404 หรือไม่
-        // xxxxx
+        // expect(res.xxxxx).xxxxx(xxxxx);
 
         // // ตรวจสอบการแก้ไขข้อมูลโปรไฟล์ : ใน res.text ที่ได้มานั้น ต้องเป็น 'User not found'
-        // xxxxx
+        // expect(res.text).xxxxx('xxxxx');
     });
 
     // ทดสอบการลบโปรไฟล์ผู้ใช้
     test('should delete the user profile', async () => {
         // // // Call API เพื่อลบโปรไฟล์
-        // const xxxxx = await xxxxx(xxxxx)
-        //     .xxxxx('xxxxxx')
+        // const res = await xxxxx(xxxxx)
+        //     .delete('/myProfile')
         //     .set('Authorization', `Bearer ${accessToken}`);
 
         // // ตรวจสอบการลบโปรไฟล์ : ว่าขึ้น statusCode 200 หรือไม่
-        // xxxxx
+        // expect(res.xxxxx).xxxxx(xxxxx);
 
         // // ตรวจสอบการลบโปรไฟล์ : ใน res.text ที่ได้มานั้น ต้องเป็น 'User deleted successfully'
-        // xxxxx
+        // expect(res.text).xxxxx('xxxxx');
     });
 
     // ทดสอบการลบโปรไฟล์ผู้ใช้ ที่ไม่มีอยู่จริง
@@ -168,32 +171,35 @@ describe('User Management API', () => {
         // xxxxxx
 
         // // Call API เพื่อลบโปรไฟล์
-        // xxxxx
+        // const res = await xxxxx(xxxxx)
+        //     .delete('/myProfile')
+        //     .set('Authorization', `Bearer ${accessToken}`);
 
         // // ตรวจสอบการลบโปรไฟล์ : ว่าขึ้น statusCode 404 หรือไม่
-        // xxxxx
+        // expect(res.xxxxx).xxxxx(xxxxx);
 
         // // ตรวจสอบการลบโปรไฟล์ : ใน res.text ที่ได้มานั้น ต้องเป็น 'User not found'
-        // xxxxx
+        // expect(res.text).xxxxx('xxxxx');
     });
 });
 
 describe('Token Authentication', () => {
     it('should return 401 if token is missing', async () => {
         // // Call API เพื่อดูข้อมูลโปรไฟล์
-        // xxxxx
+        // const res = await xxxxx(xxxxx)
+        //     .get('/myProfile');
 
         // // ตรวจสอบการดูข้อมูลโปรไฟล์ : ว่าขึ้น statusCode 401 หรือไม่
-        // xxxxx
+        // expect(res.xxxxx).xxxxx(xxxxx);
     });
 
     it('should return 403 if token is invalid', async () => {
         // // Call API เพื่อดูข้อมูลโปรไฟล์
-        // const xxxxx = await xxxxx(xxxxx)
-        //     .xxxxxx('xxxxx')
+        // const res = await xxxxx(xxxxx)
+        //     .get('/myProfile')
         //     .set('Authorization', 'Bearer invalidToken'); // ส่ง token ที่ไม่ถูกต้อง
 
         // ตรวจสอบการดูข้อมูลโปรไฟล์ : ว่าขึ้น statusCode 403 หรือไม่
-        // xxxxx
+        // expect(res.xxxxx).xxxxx(xxxxx);
     });
 });
